535d152e117350a8e389768849e8ac2813d3ca05
Edicher Escol <edicherponce.escol@gmail.com>
2023-11-20 09:51:39 +0800

    fixed portfolio, added learnings

--- js/script.js
+++ js/script.js
@@ -7,6 +7,22 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
+/*========== swiper ==========*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
